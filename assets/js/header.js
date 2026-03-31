dd1c428e4cb1008bb8722e17f2b2fa6ac84a4b6b
DCCuong <cuongfc1231232005@gmail.com>
2026-03-31 21:40:06 +0700

    trang trí thêm cho trang, sửa lỗi 404, sửa lỗi ảnh lỗi dẫn  src js

---
+++ assets/js/header.js
@@ -0,0 +1,25 @@
+
+//.........HEADER
+const toggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
+
+toggle.onclick = () => {
+  menu.classList.toggle("active");
+};
+// cuộn trang thì tắt header
+let lastScroll = 0;
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  let currentScroll = window.pageYOffset;
+  if (currentScroll > lastScroll) {
+    // scroll xuống
+    header.classList.add("hide");
+    menu.classList.remove("active");
+  } else {
+    // scroll lên
+    header.classList.remove("hide");
+  }
+
+  lastScroll = currentScroll;
+});
